38ad80d968168849156761a5eddda7e61bce8dd9
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2011-05-09 16:52:59 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepotOnja::Application.config.secret_token = '859bcea1537077ea1779edb60092f10d4b59ebb5fd96063d2f66950a9a172065a368ce7f0081f57ebd5be9079e374cb9cd3fe76e3e95e9afce41e628b7a61af3'
