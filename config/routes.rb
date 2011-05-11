b2657fb09385fb144148ef1f4131c63b34df2e31
onja <andriamasinoroonja@yahoo.fr>
2011-05-11 16:37:45 +0300

    part II task C

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 DepotOnja::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
   get "file_m_anage/getFileContent"
 
   get "say/hello"
 
-  get "say/goodbye"
+  get "say/goodbye" 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,7 +59,7 @@ DepotOnja::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  root :to => 'store#index', :as => 'store'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
