7e8e420cf74aeb61bd5b86485e74c6c4e38552e8
onja <andriamasinoroonja@yahoo.fr>
2011-05-12 17:58:46 +0300

    part II taskD E F

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
-  def index
-    @products = Product.all
+  def index    
+    @counter = 1    
+    if session[:counter].nil?      
+      session[:counter] = @counter      
+    else            
+      @counter = session[:counter] += 1
+    end    
+    @products = Product.all    
+    @cart = current_cart
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,21 @@ class Product < ActiveRecord::Base
                                       :with => %r{\.(jpeg|jpg|gif|png)$}i,
                                       :message => 'Verify your image type'
                                     }
+                                    
+                                    
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  private 
+  def ensure_not_referenced_by_any_line_item
+    
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line items present')
+      return false
+    end
+    
+  end
+
 end
